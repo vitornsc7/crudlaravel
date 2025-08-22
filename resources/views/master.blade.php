f0b76a2c70b8976b3c44bc6be8d9e364b103d890
Vitor Nascimento <vitornsc7@gmail.com>
2025-08-22 12:24:43 -0300

    Adição do Tailwind ao projeto e página de usuários estilizada

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,13 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Crud com Laravel</title>
+    <title>Gestão de Usuários</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body>
-    <div class="container">
-        @yield('content')
-    </div>
+<body class="antialiased bg-gray-200">
+    @yield('content')
 </body>
 
 </html>
